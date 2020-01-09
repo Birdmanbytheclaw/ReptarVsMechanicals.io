2aba8ee3cafb73e914390cc943fcc033259d7afc
Birdmanbytheclaw <therealparkerflynn@gmail.com>
2020-01-09 14:44:16 -0600

    halo

---
+++ BotsVsDino/Dinosaur.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotsVsDino
+{
+    class Dinosaur
+    {
+        public double DinoHealth;
+        public bool DinoEnergy;
+        public double DinoAtkPower;
+
+        
+    }
+}
---
+++ BotsVsDino/Robot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotsVsDino
+{
+    class Robot
+    {
+        public string Name;
+        public double Health;
+        public double  YourPowerLevel;
+        public string Weapon;
+        public double WeaponPowerLevel;
+
+    }
+}
